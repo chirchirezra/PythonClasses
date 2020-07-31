13ced6937c0fd378c93054532d7d7ec712aa6b94
Ezra Chirchir <ezrachirchir7477@gmail.com>
2020-07-31 14:12:44 +0300

    Working with  more json conf files

--- working_with_json.py
+++ working_with_json.py
@@ -2,4 +2,11 @@ import json
 
 with open("config.json") as json_file:
     json_data = json.load(json_file)
-    print(json_data)
+   # print(json_data)
+json_data['db_host'] = "localhost"
+json_data['db_name'] = "WorkingWithPythonClasses"
+json_data['db_user'] = "root"
+
+with open("config.json",'w') as  fw:
+    
+    json.dump(json_data,fw)
